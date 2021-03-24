7ef083ed25519b004242977956504bc46fc3ddb5
Sean Ottomanelli <sean.ottomanelli@gmail.com>
2021-03-24 16:12:01 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {
+    name : 'Sean',
+    streetAdress: '111 West 77th Street'
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return Object.assign({}, obj, { [key]: value});
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    return Object.assign(obj, { [key]: value});
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmployee = { ...obj };
+    delete newEmployee.[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj.[key];
+    return obj;
+}
